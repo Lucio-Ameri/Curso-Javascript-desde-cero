5074931f946d2d50a6574bf5b5576d218a589c16
Lucio Ameri <lucio.ameri18@gmail.com>
2026-04-29 15:03:59 -0300

    Finalizo el desafio del modulo 1

---
+++ Modulo 1/Desafio/js/main.js
@@ -0,0 +1,4 @@
+let nombreUsuario = prompt("INGRESE SU NOMBRE: ");
+let apellidoUsuario = prompt("INGRESE SU APELLIDO: ");
+
+alert("Hola " + nombreUsuario + " " + apellidoUsuario + ". Bienvenido a la aplicación!");
